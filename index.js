3dafbf2b97c2929a20f9658e7f450de606cc937a
Robert Madureira <robeet73@gmail.com>
2022-02-19 17:14:01 -0300

    ok

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const path = require("path");
 const app = express();
 
 app.use('/api', apiRoute);
-app.use('/', express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-    console.log("Server running on port", PORT)
-})
+app.listen(PORT, () => console.log("Server running on port", PORT));
 
