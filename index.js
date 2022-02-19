fc650120b36dcdfea5ac30e3e2f74a57fa17f0c8
Robert Madureira <robeet73@gmail.com>
2022-02-19 14:58:36 -0300

    post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const PORT = 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
+const res = require('express/lib/response');
 
 const app = express();
 
@@ -15,11 +16,24 @@ let posts = [
 app.get("/all", (req, res) => { 
 
     res.json(JSON.stringify(posts))
-    
+
 });
 
-app.post("/new", (req, ress) => { });
+app.post("/new", bodyParser.json(), (req, res) => { 
+    let id = generateID();
+    let title = req.body.title;
+    let desscription = req.body.description;
+
+    posts.push({id, title, desscription})
+
+    res.send("Post adicionado");
+
+});
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
 })
+
+function generateID(){
+    return Math.random().toString(36).substr(2, 9);
+}
